d0c42f624b45adbfc30a208153e6a14beb1f222a
AkshayaPk <ashpeekay23@gmail.com>
2018-06-19 13:26:33 -0400

    Edited Utility Class Check

--- src/main/java/com/pluralsight/HelloWorld.java
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -1,6 +1,6 @@
 package com.pluralsight;
 
-public class HelloWorld {
+final public class HelloWorld {
 public static void main(final String[] args) {
 System.out.println("Hello World");
 System.out.println("Added Git Integration");
