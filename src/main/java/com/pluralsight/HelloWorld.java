6016dd432423fb3d2fddfb24ef678c5e73f6273f
AkshayaPk <ashpeekay23@gmail.com>
2018-06-19 13:30:18 -0400

    Edited Utility Class Check once more

--- src/main/java/com/pluralsight/HelloWorld.java
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -1,6 +1,8 @@
 package com.pluralsight;
 
 public final class HelloWorld {
+private HelloWorld(){
+}
 public static void main(final String[] args) {
 System.out.println("Hello World");
 System.out.println("Added Git Integration");
