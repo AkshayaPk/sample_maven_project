9cda7e02e278a68a067db4e7ca54a1b434c3b487
AkshayaPk <ashpeekay23@gmail.com>
2018-06-19 13:33:14 -0400

    Edited Utility Class Check once more

--- src/main/java/com/pluralsight/HelloWorld.java
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -1,7 +1,7 @@
 package com.pluralsight;
 
 public final class HelloWorld {
-private HelloWorld(){
+private HelloWorld() {
 }
 public static void main(final String[] args) {
 System.out.println("Hello World");
