5fbb3cbbb5aa9643874bb0a6d020fe58f6ed4b28
AkshayaPk <ashpeekay23@gmail.com>
2018-06-19 13:20:09 -0400

    Edited some code checks

--- src/main/java/com/pluralsight/HelloWorld.java
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -1,8 +1,8 @@
 package com.pluralsight;
 
 public class HelloWorld {
-public static void main(String[] args) {
-	System.out.println("HelloWorld");
+public static void main(final String[] args) {
+	System.out.println("Hello World");
 	System.out.println("Added Git Integration");
 }
 }
